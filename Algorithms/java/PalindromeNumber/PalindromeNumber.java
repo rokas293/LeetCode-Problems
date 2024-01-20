8d17e1d20b4d5fc86db5f3ada1eceaefc525b8e6
rokas mitka <83891594+rokas293@users.noreply.github.com>
2024-01-20 13:50:35 +0000

    Update PalindromeNumber.java

--- Algorithms/java/PalindromeNumber/PalindromeNumber.java
+++ Algorithms/java/PalindromeNumber/PalindromeNumber.java
@@ -1,1 +1,51 @@
+// Source : https://leetcode.com/problems/palindrome-number/
+// Author : Rokas Mitka
+// Date   : 2024-01-20
 
+/***************************************************************************************************************************
+* Given an integer x, return true if x is a 
+* palindrome
+* , and false otherwise.
+* 
+*  
+* 
+* Example 1:
+* 
+* Input: x = 121
+* Output: true
+* Explanation: 121 reads as 121 from left to right and from right to left.
+* Example 2:
+* 
+* Input: x = -121
+* Output: false
+* Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+* Example 3:
+* 
+* Input: x = 10
+* Output: false
+* Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+*  
+* 
+* Constraints:
+* 
+* -231 <= x <= 231 - 1
+***************************************************************************************************************************/
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        long reversed = 0;
+        long temp = x;
+
+        while(temp != 0) {
+            int digit = (int) temp % 10;
+            reversed = reversed * 10 + digit;
+            temp /= 10;
+        }
+
+        return reversed == x;
+    }
+}
