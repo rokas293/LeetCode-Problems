2b813af052fd2687389c34fe1e4fa833ec6b1db2
rokas mitka <83891594+rokas293@users.noreply.github.com>
2024-06-17 20:15:58 +0100

    Update LongestCommonPrefix.java

--- Algorithms/java/LongestCommonPrefix/LongestCommonPrefix.java
+++ Algorithms/java/LongestCommonPrefix/LongestCommonPrefix.java
@@ -29,13 +29,17 @@
 
 class Solution {
     public String longestCommonPrefix(String[] strs) {
-        if(strs.length == 0) return "";
-        String prefix = strs[0];
-        for(int i = 1; i < strs.length; i++) {
-            while(strs[i].indexOf(prefix) != 0) {
-                prefix = prefix.substring(0, prefix.length()-1);
+        Arrays.sort(strs);
+        String s1 = strs[0];
+        String s2 = strs[strs.length-1];
+        int idx = 0;
+        while(idx < s1.length() && idx < s2.length()) {
+            if(idx < s1.length() && s1.charAt(idx) == s2.charAt(idx)){
+                idx++;
+            } else {
+                break;
             }
         }
-        return prefix;
+        return s1.substring(0, idx);
     }
 }
