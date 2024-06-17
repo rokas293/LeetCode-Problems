81f5c86f4d2fc2d7a919ceea3c56d9c07e1b1c4e
rokas mitka <83891594+rokas293@users.noreply.github.com>
2024-06-17 20:15:21 +0100

    Update TwoSum.java

--- Algorithms/java/TwoSum/TwoSum.java
+++ Algorithms/java/TwoSum/TwoSum.java
@@ -32,17 +32,14 @@
  ****************************************************************************************************************************/
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        for (int i=0; i<nums.length; i++) {
-            for (int j=i+1; j<nums.length; j++) {
-                int complement = target - nums[i];
-
-                 if (nums[j] == complement) {
-                     return new int[] { i, j };
-                 }
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
             }
         }
-
-        throw new IllegalArgumentException("no match found");
+        return new int[]{}; // No solution found
     }
 }
