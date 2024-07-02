7d71676a87a9ba5b9656d8f4ec79ef8475647fcb
rokas mitka <83891594+rokas293@users.noreply.github.com>
2024-07-02 14:29:33 +0100

    Update PascalsTriangle.java

--- Algorithms/java/PascalsTriangle/PascalsTriangle.java
+++ Algorithms/java/PascalsTriangle/PascalsTriangle.java
@@ -1,1 +1,56 @@
+// Source : https://leetcode.com/problems/pascals-triangle/
+// Author : Rokas Mitka
+// Date   : 2024-07-02
 
+/****************************************************************************************************
+* Given an integer numRows, return the first numRows of Pascal's triangle.
+* 
+* In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+* 
+* 
+* Example 1:
+* 
+* Input: numRows = 5
+* Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+* 
+* Example 2:
+* 
+* Input: numRows = 1
+* Output: [[1]]
+*  
+* 
+* Constraints:
+* 
+* 1 <= numRows <= 30
+****************************************************************************************************/
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        
+        List<List<Integer>> triangle = new ArrayList<>();
+
+        if (numRows == 0) {
+            return triangle;
+        }
+
+        List<Integer> first_row = new ArrayList<>();
+        first_row.add(1);
+        triangle.add(first_row);
+
+        for (int i = 1; i < numRows; i++) {
+            List<Integer> prev_row = triangle.get(i-1);
+            List<Integer> row = new ArrayList<>();
+
+            row.add(1);
+
+            for (int j = 1; j < i; j++) {
+                row.add(prev_row.get(j-1) + prev_row.get(j));
+            }
+
+            row.add(1);
+            triangle.add(row);
+        }
+
+        return triangle;
+    }
+}
