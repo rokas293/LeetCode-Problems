431d74a4cc08a709161b540655869213b1a5d057
rokas mitka <83891594+rokas293@users.noreply.github.com>
2024-08-04 14:56:24 +0100

    Update NumberOf1Bits.java

--- Algorithms/java/NumberOf1Bits/NumberOf1Bits.java
+++ Algorithms/java/NumberOf1Bits/NumberOf1Bits.java
@@ -1,1 +1,60 @@
+// Source : https://leetcode.com/problems/number-of-1-bits/
+// Author : Rokas Mitka
+// Date   : 2024-08-04
 
+/******************************************************************************************************************************************
+* Write a function that takes the binary representation of a positive integer and returns the number of 
+* set bits
+* it has (also known as the Hamming weight).
+* 
+*  
+* 
+* Example 1:
+* 
+* Input: n = 11
+* 
+* Output: 3
+* 
+* Explanation:
+* 
+* The input binary string 1011 has a total of three set bits.
+* 
+* Example 2:
+* 
+* Input: n = 128
+* 
+* Output: 1
+* 
+* Explanation:
+* 
+* The input binary string 10000000 has a total of one set bit.
+* 
+* Example 3:
+* 
+* Input: n = 2147483645
+* 
+* Output: 30
+* 
+* Explanation:
+* 
+* The input binary string 1111111111111111111111111111101 has a total of thirty set bits.
+* 
+*  
+* 
+* Constraints:
+* 
+* 1 <= n <= 231 - 1
+******************************************************************************************************************************************/
+
+class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            count += n & 1;
+            n >>>= 1; // right unsigned shift
+        }
+
+        return count;
+    }
+}
+ 
